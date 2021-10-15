035391763b8c4fea18fdf9a3be61e6ac9d64dc18
Sai Likhith Adari <kesavlikku@gmail.com>
2021-10-15 20:29:35 +0530

    1st commit

--- Wifi_pass/havk.py
+++ Wifi_pass/havk.py
@@ -16,10 +16,7 @@ class Wifi:
         for i in range(len(profiles)):
             profile_results = sp.getstatusoutput(cmd + f" \"{profiles[i]}\" key=clear")
             passwd = [j.split(':')[1][1:] for j in profile_results[1].split("\n") if "Key Content" in j]
-            if len(passwd) == 0:
-                pass
-            else:
-                content = [i + 1, profiles[i], passwd[0]]
-                worksheet.write_row(i + 1, 0, content)
-                print(f"File named {fileName} is stored at {os.getcwd()}")
+            content = [i + 1, profiles[i], passwd[0]]
+            worksheet.write_row(i + 1, 0, content)
+        print(f"File named {fileName} is stored at {os.getcwd()}")
         workbook.close()
